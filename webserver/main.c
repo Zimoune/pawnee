e7860dd407302dac94c397bfb7a892cbea0e8ddd
Tanguy DEPOMMIER <tanguy.depommier@etudiant.univ-lille1.fr>
2015-01-28 09:37:40 +0100

    Commentaire dans le code + git ignore

--- webserver/main.c
+++ webserver/main.c
@@ -6,10 +6,14 @@
 int main(void)
 {
 	
+	/* Creer le serveur */
 	int fd_serveur = creer_serveur(8000);
+
+
 
 	while(1) {
 		int socket_client;
+		/* Accepete la connection du client */
 		socket_client = accept(fd_serveur, NULL, NULL);
 		if (socket_client == -1) {
 			perror("Accept");
@@ -17,13 +21,14 @@ int main(void)
 		}
 		const char *message_bienvenue = "Bonjour, bienvenue ! \n";
 
-
+		/* Envoie le message de bienvenue */
 		write(socket_client, message_bienvenue, strlen(message_bienvenue));
 
 		char buf[512];
 
 		int end = read(socket_client, buf, 512);
 
+		/* Ecoute ce qu'il se passe et renvoie a tout les clients */
 		while (end != 0) {
 			write(socket_client, buf, end);
 			end = read(socket_client, buf, 512);
--- webserver/socket.c
+++ webserver/socket.c
@@ -6,6 +6,8 @@ int creer_serveur(int port){
 
 	int socket_serveur;
 
+
+	/* Creation de la socket serveur */
 	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (socket_serveur == -1) {
@@ -13,17 +15,23 @@ int creer_serveur(int port){
 		return -1;
 	}		
 
+
+
 	struct sockaddr_in saddr;
 	saddr.sin_family = AF_INET;
 	saddr.sin_port = htons(port);
 	saddr.sin_addr.s_addr = INADDR_ANY;
 
+
+	/* Attachement de la socket server sur toute les interfaces */
 	if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
 	{
 		perror("bind socket_serveur");
 		return -1;
 	}
 
+
+	/* Demarre l'attente de connection */
 	if (listen(socket_serveur, 10) == -1)
 	{
 		perror("listen socket_serveur");
